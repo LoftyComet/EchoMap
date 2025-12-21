79f3329f1809cbfa5490563e92b2579a970d67de
Ku1yin <94165831+ku1yin@users.noreply.github.com>
2025-12-21 13:27:05 +0800

    Revert "debug"
    This reverts commit ef46b251c7733e3ecb43e61d272f49af27ddb185.

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,2 +1,2 @@
 from .user import User, UserCreate, UserBase
-from .audio import AudioRecord, AudioRecordCreate, AudioRecordBase, AudioRecordUpdate
+from .audio import AudioRecord, AudioRecordCreate, AudioRecordBase
